caf5c98c2c67a101c1411c86477909f090d91a82
Gyan-02 <gyangaurav02@gmail.com>
2025-07-21 10:16:25 +0530

    Create App.js

---
+++ Backend/App.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const errorhandler = require("./middleware/errorhandler");
+const userRouter = require('./routes/userRouter');
+const categoryRouter = require('./routes/categoryRouter');
+const transactionRouter = require('./routes/transactionRouter');
+const port =3000;
+const app= express();
+
+mongoose.connect("mongodb+srv://user1:pass1@cluster1.ddwffa4.mongodb.net/Stacks")
+.then(()=> console.log("DB_CONNECTED"))
+.catch((e)=> console.log((e)))
+
+app.use(cors({origin:["http://localhost:5173"]}))
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Welcome to BudgetWise API");
+});
+app.use("/api/v1/users",userRouter);
+app.use("/api/v1/categories",categoryRouter);
+app.use("/api/v1/transactions",transactionRouter);
+
+app.use(errorhandler);
+
+
+app.listen(port,()=>{
+    console.log(`The server is running on ${port}`)
+})
